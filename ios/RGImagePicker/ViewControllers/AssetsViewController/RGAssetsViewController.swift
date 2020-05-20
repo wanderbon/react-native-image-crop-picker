cd4cf3c79b205683e2ee2cc00a80d376cf1e41f3
Alexander Blokhin <sashablokhindev@gmail.com>
2020-05-20 20:54:09 +0300

    Update RGAssetsViewController.swift

--- ios/RGImagePicker/ViewControllers/AssetsViewController/RGAssetsViewController.swift
+++ ios/RGImagePicker/ViewControllers/AssetsViewController/RGAssetsViewController.swift
@@ -334,7 +334,7 @@ class RGAssetsViewController: UICollectionViewController {
         
         if let assetCollection = self.assetCollection {
             let options = PHFetchOptions()
-            options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+            options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
             
             switch (imagePickerController.mediaType) {
             case .RGImagePickerMediaTypeImage:
